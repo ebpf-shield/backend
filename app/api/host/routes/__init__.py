2ec67abdb171c35fba900ba5b035d23b2c785053
Liel Almog <liel.almogg@gmail.com>
2025-04-09 23:27:45 +0300

    Feat: Add the host router

--- app/api/host/routes/__init__.py
+++ app/api/host/routes/__init__.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
-
+from .process_route import router as process_router
 
 api_router = APIRouter(prefix="/host", tags=["host"])
+
+api_router.include_router(process_router, prefix="/process", tags=["process"])
--- app/core/app.py
+++ app/core/app.py
@@ -7,7 +7,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.errors.validation_error import request_validation_exception_handler
 from app.core.db import mongo_client_manager
 from app.core.logger import setup_logger
-from ..api.ui.routes import api_router
+from ..api.ui.routes import api_router as ui_api_router
+from ..api.host.routes import api_router as host_api_router
 
 
 @asynccontextmanager
@@ -20,7 +21,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="ebShield", lifespan=lifespan)
 app.add_exception_handler(RequestValidationError, request_validation_exception_handler)
-app.include_router(router=api_router, prefix="/api")
+app.include_router(router=ui_api_router, prefix="/api")
+app.include_router(router=host_api_router, prefix="/api")
+app.add_api_route(
+    "/openapi.json",
+    include_in_schema=False,
+    endpoint=lambda: app.openapi(),
+)
 
 
 origins = [
