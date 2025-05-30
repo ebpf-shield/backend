677805853e880c2946d8ee8d2ed28f40f4ac8ca9
Liel Almog <liel.almogg@gmail.com>
2025-05-30 19:01:42 +0300

    Add an internal server error expection

--- app/api/ui/routes/organization_route.py
+++ app/api/ui/routes/organization_route.py
@@ -2,6 +2,11 @@ from typing import Annotated
 from beanie import PydanticObjectId
 from fastapi import APIRouter, Body, Path
 
+from app.api.errors.conflict_exception import ConflictException
+from app.api.errors.internal_server_error import IntervalServerErrorException
+from app.api.errors.no_user_with_email_exception import NoUserWithEmailException
+from app.api.errors.not_found_exception import NotFoundException
+from app.api.errors.user_have_organization import UserHaveOrgException
 from app.api.models.organization_model import (
     CreateOrganizationDTO,
     OrganizationInvitationDTO,
@@ -28,7 +33,7 @@ async def create(
     description="Get organization by id",
 )
 async def find_by_id(
-    organization_id: Annotated[PydanticObjectId, Path(description="Process id")],
+    organization_id: Annotated[PydanticObjectId, Path(description="Organization id")],
     organization_service: CommonUIOrganizationService,
 ):
     return await organization_service.find_by_id(organization_id)
@@ -36,8 +41,15 @@ async def find_by_id(
 
 @router.post("/{organization_id}/invitation")
 async def create_invitation(
-    organization_id: Annotated[PydanticObjectId, Path(description="Process id")],
+    organization_id: Annotated[PydanticObjectId, Path(description="Organization id")],
     body: Annotated[OrganizationInvitationDTO, Body()],
     organization_service: CommonUIOrganizationService,
 ):
-    return await organization_service.create_invitation(organization_id, body.email)
+    try:
+        return await organization_service.create_invitation(organization_id, body.email)
+    except NoUserWithEmailException as e:
+        raise NotFoundException("User with this email does not exist") from e
+    except UserHaveOrgException as e:
+        raise ConflictException("User already have organization") from e
+    except Exception as _e:
+        raise IntervalServerErrorException
